cbb0724312d464754f6cb54d40423a75fc720539
Dom Mariano <dom.cpa.ano@gmail.com>
2015-02-18 22:19:32 +0800

    [TASK] Bump up version number to 0.3.0.
     * Release 0.3.0 for testing on local src.wordpress-develop.dev,
       geoipsl.wpengine.com and firebirds.staging.wpengine.com.

--- geoip-site-locations.php
+++ geoip-site-locations.php
@@ -6,7 +6,7 @@ error_reporting( E_ALL );
 Plugin Name: GeoIP Site Locations
 Description: Detect user location based on IP or cookie information and redirect
 to the appropriate geo-targetted version of your site.
-Version: 0.2.0
+Version: 0.3.0
 Author: Dominique Mariano
 Author URI: http://www.twitter.com/miniQueue
 License: GPLv2 or later
