c813f2edfaf2fc7d7e495204d7a29531d3f0660f
Dom Mariano <dom.cpa.ano@gmail.com>
2015-05-19 10:39:47 -0400

    [TASK] Rename shortcodes.

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -5,8 +5,8 @@
  * @since 0.4.0
  */
 
-add_shortcode( 'geoipsl_remember_last_site', 'geoipsl_remember_last_site_form' );
-function geoipsl_remember_last_site_form( $atts ) {
+add_shortcode( 'geoipsl_remember_me', 'geoipsl_remember_me_form' );
+function geoipsl_remember_me_form( $atts ) {
   global $geoipsl_admin_settings;
   global $geoipsl_settings;
 
@@ -18,25 +18,25 @@ function geoipsl_remember_last_site_form( $atts ) {
 
   $output  = '<input ';
   $output .= 'type="checkbox" ';
-  $output .= 'name="geoipsl-record-last-visit" ';
-  $output .= 'id="geoipsl-record-last-visit" ';
+  $output .= 'name="geoipsl-remember-me" ';
+  $output .= 'id="geoipsl-remember-me" ';
   $output .= sprintf( 'class="%s" ', esc_attr( $class ) );
   $output .= '>';
 
   $before  = '<form ';
-  $before .= 'class="geoipsl-record-last-visit-form" ';
-  $before .= 'name="geoipsl-record-last-visit-form" ';
+  $before .= 'class="geoipsl-remember-me-form" ';
+  $before .= 'name="geoipsl-remember-me-form" ';
   $before .= '>';
 
-  $before  = apply_filters( 'geoipsl_remember_last_site_before', $before );
+  $before  = apply_filters( 'geoipsl_remember_me_before', $before );
 
   $after   = '<label ';
-  $after  .= 'for="geoipsl-record-last-visit">';
+  $after  .= 'for="geoipsl-remember-me">';
   $after  .= __( 'Remember this location', 'geoipsl' );
   $after  .= '</label>';
   $after  .= '</form>';
 
-  $after  = apply_filters( 'geoipsl_remember_last_site_after', $after );
+  $after  = apply_filters( 'geoipsl_remember_me_after', $after );
 
   $output = $before . $output . $after;
 
@@ -52,11 +52,18 @@ function geoipsl_remember_last_site_form( $atts ) {
 
 add_shortcode( 'geoipsl_suggest_closest_site', 'geoipsl_suggest_closest_site' );
 function geoipsl_suggest_closest_site( $atts ) {
+
+  $args = shortcode_atts( array(
+    'label' => __( 'Closest Site', 'geoipsl' ),
+  ), $atts );
+
+  extract( $args );
+
   $before  = '<a href="" class="geoipsl-closest-site-link">';
   $after   = '</a>';
   $after  .= '<script type="text/javascript" src="' . trailingslashit( plugin_dir_url( __FILE__ ) ) . 'js/geoipsl-nearest-site.js' . '"></script>';
 
-  $output = $before . __( 'Closest Site', 'geoipsl' ) . $after;
+  $output = $before . $label . $after;
 
   return $output;
 }
